59ab143dc29eb5fac8208474ec2372e403d4ae88
Pepesaur96 <jjvazquez96@gmail.com>
2023-06-16 01:52:46 +0000

    Hello, puts.hotfix.2

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,8 +2,7 @@
 #include <stdio.h>
 int main()
 {
-    fputs("\"Programming is like building a multilingual puzzle", stdout);
-    fputs();
+    puts("\"Programming is like building a multilingual puzzle", stdout);
 
     getchar();
     return 0;
