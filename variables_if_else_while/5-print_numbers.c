005dcdb93da83e8dfb6c2c5aeb80b2cae589ffaf
Pepesaur96 <jjvazquez96@gmail.com>
2023-06-20 01:52:43 +0000

    Numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ *main - print 0-9 with a new line after
+ *Return: always !0
+ */
+
+int main(void)
+{
+	int x = 0;
+
+	while (x < 10)
+	{
+		putchar(x + '0');
+		if (x < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		x++;
+	}
+	putchar('\n');
+
+	return (0);
+}
