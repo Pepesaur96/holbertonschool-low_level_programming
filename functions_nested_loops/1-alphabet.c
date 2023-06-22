0c8b45462811f180a3ea4f37d25ede4c9873d31d
Pepesaur96 <jjvazquez96@gmail.com>
2023-06-22 14:20:09 +0000

    I sometimes suffer from insomnia.hotfix.2.3

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,8 +1,8 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * main - display a-z followed by new line
- * print_alphabet - var for printing
+ * print_alphabet - display a-z followed by new line
+ * @alphabet: var for printing
  * alphabet - definition of var
  * putchar - prints
  * Return: always 0 success
