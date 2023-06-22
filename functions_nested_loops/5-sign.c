e39826dc65315fdc558821e88cd1af5da1355f46
Pepesaur96 <jjvazquez96@gmail.com>
2023-06-22 18:45:39 +0000

    Sign.hotfix

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -9,10 +9,10 @@
 int print_sign(int n)
 {
 	if (n > 0)
-		putchar('+');
+		_putchar('+');
 	else if (n < 0)
-		putchar('-');
+		_putchar('-');
 	else
-		putchar('0');
+		_putchar('0');
 	return (0);
 }
