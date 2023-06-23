e6571e72e2794e41d51d95ab93297e3a193948f6
Pepesaur96 <jjvazquez96@gmail.com>
2023-06-23 18:14:43 +0000

    Learn your times table.hotfix

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,31 +1,21 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * times_table - prints the multiplication table #9
- * Return: always !0
+ * times_table - prints the multiplication table starting with 0
  */
 
 void times_table(void)
 {
-	int x, y, z;
+	int row, lines;
 
-	for (x = 0; x <= 9; x++)
+	for (lines = 0; lines < 10; lines++)
 	{
-		_putchar('0');
-
-		for (y = 1; y <= 9; y++)
+		for (row = 0; row < 10; row++)
 		{
+			_putchar((row * lines) / 10 + '0');
+			_putchar((row * lines) % 10 + '0');
 			_putchar(',');
 			_putchar(' ');
-
-			z = x * y;
-
-			if (z <= 9)
-				_putchar(' ');
-			else
-				_putchar((y / 10) + '0');
-
-			_putchar((y % 10) + '0');
 		}
 		_putchar('\n');
 	}
