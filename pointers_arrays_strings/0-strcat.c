8460e9d61fc9977b56d06de9f398cdf8f5fdf8ed
Pepesaur96 <jjvazquez96@gmail.com>
2023-06-28 19:35:34 +0000

    strcat

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strcat - joins two strings
+ *@dest: string
+ *@src: input string
+ *Return: pointer to dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+int i = 0, j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
