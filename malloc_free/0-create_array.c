4d0e910bcfdb0a7329b04ec4ddca3a995a91c649
Pepesaur96 <jjvazquez96@gmail.com>
2023-07-13 17:28:03 +0000

    bbranchadd

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <string.h>
+/**
+ * create_array - create the array to be printed
+ * malloc - returnd value to the char pointer
+ * @c: is the array to be printed
+ * @size: lenght of array
+ * Return: array for success
+ */
+
+char *create_array(unsigned int size, char c)
+{
+
+	char *array = NULL;
+
+	if (size == 0)
+		return (NULL);
+
+	array = malloc(sizeof(char) * size);
+
+	if (array == NULL)
+		return (NULL);
+
+	memset(array, c, size);
+	return (array);
+}
