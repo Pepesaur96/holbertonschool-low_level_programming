86a3cd4fadb6ee915f175d7e582afb6de2dc4d02
Pepesaur96 <jjvazquez96@gmail.com>
2023-07-19 15:13:20 +0000

    bbranchadd

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - new struct def
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of owner
+ */
+
+typedef struct dog
+{
+char *name;
+float age;
+char *owner;
+} dog_t;
+
+#endif
