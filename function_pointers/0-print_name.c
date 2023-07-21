88c8b5050cf39330503247f8a1fa17ce46f3a220
Pepesaur96 <jjvazquez96@gmail.com>
2023-07-21 16:50:39 +0000

    What'smy name

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * print_name - uses *f function to print name
+ * @name: char
+ * @f: pointer to function
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
