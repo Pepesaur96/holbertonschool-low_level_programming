f9acce9f9f30f94ad103c4941dfaa9645bfb17d7
Pepesaur96 <jjvazquez96@gmail.com>
2023-08-09 19:22:37 +0000

    header and 0file

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * ssize_t read_textfile - reads a text file and displays it to stdOUT
+ * @filename: pointer to txt file
+ * @letters: # of letter it should read & print
+ * Return: #of characters printed or "0" if failed
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+ssize_t open_file, read_file, write_file;
+char *text;
+
+text = malloc(letters);
+
+	if (text == NULL)
+		return (0);
+
+	if (filename == NULL)
+		return (0);
+
+open_file = open(filename, O_RDONLY);
+
+	if (open_file == -1)
+	{
+		free(text);
+		return (0);
+	}
+
+read_file = read(open_file, text, letters);
+
+write_file = write(STDOUT_FILENO, text, read_file);
+
+close(open_file);
+
+return (write_file);
+}
