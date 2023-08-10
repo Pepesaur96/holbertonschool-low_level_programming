54c8f169e912a8c6611a5e93f90e1408e2e80987
Pepesaur96 <jjvazquez96@gmail.com>
2023-08-10 16:45:28 +0000

    betty.2

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -7,19 +7,19 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-unsigned long int i = 0;
-hash_table_t *new_table;
+	unsigned long int i = 0;
+	hash_table_t *new_table;
 
 	if (size == 0)
 		return (NULL);
 
-new_table = (hash_table_t *)malloc(sizeof(hash_table_t));
+	new_table = (hash_table_t *)malloc(sizeof(hash_table_t));
 
 	if (new_table == NULL)
 		return (NULL);
 
-new_table->size = size;
-new_table->array = (hash_node_t **)malloc(size * sizeof(hash_node_t *));
+	new_table->size = size;
+	new_table->array = (hash_node_t **)malloc(size * sizeof(hash_node_t *));
 
 	if (new_table->array == NULL)
 	{
@@ -29,8 +29,8 @@ new_table->array = (hash_node_t **)malloc(size * sizeof(hash_node_t *));
 
 	while (i < size)
 	{
-	new_table->array[i] = NULL;
+		new_table->array[i] = NULL;
 	}
 
-return (new_table);
+	return (new_table);
 }
