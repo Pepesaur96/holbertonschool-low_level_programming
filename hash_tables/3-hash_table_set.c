7c41900cace76ca64f443e7caa3d040f6ac0109f
Pepesaur96 <jjvazquez96@gmail.com>
2023-08-11 17:02:52 +0000

    freeissue

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -30,6 +30,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	}
 
+	free(new_node->value);
+
 	new_node->value = strdup(value);
 	if (new_node->value == NULL)
 	{
