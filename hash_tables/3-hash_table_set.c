a81e52a691a07cfebf3e55c6ef69e386f2f73ad8
Pepesaur96 <jjvazquez96@gmail.com>
2023-08-11 19:27:52 +0000

    freecookies.2.betty.3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -16,32 +16,29 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (ht == NULL || key == NULL || value == NULL || strlen(key) == 0)
 		return (0);
-
 	index = key_index((const unsigned char *)key, ht->size);
-	for (current_node = ht->array[index]; current_node != NULL; current_node = current_node->next)
+	for (current_node = ht->array[index]; current_node != NULL;
+			current_node = current_node->next)
 	{
 		if (strcmp(current_node->key, key) == 0)
 		{
 			free(current_node->value);
 			current_node->value = strdup(value);
 			if (current_node->value == NULL)
-				return 0;
-			return 1;
+				return (0);
+			return (1);
 		}
 	}
-
 	new_node = malloc(sizeof(hash_node_t));
 	free(new_node->value);
 	if (new_node == NULL)
 		return (0);
-
 	new_node->key = strdup(key);
 	if (new_node->key == NULL)
 	{
 		free(new_node);
 		return (0);
 	}
-
 	new_node->value = strdup(value);
 	if (new_node->value == NULL)
 	{
@@ -49,9 +46,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(new_node);
 		return (0);
 	}
-
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
-
 	return (1);
 }
